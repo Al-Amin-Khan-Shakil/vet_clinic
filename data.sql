7af5380964ad7858a81d70235e482f8454ba8a5d
al-amin <alaminkhanshakil@gmail.com>
2023-10-27 03:44:41 +0600

    Insert value into owners table

--- data.sql
+++ data.sql
@@ -23,3 +23,13 @@ INSERT INTO species
 VALUES 
   ('Pokemon'),
   ('Digimon');
+
+INSERT INTO owners
+  (full_name, age)
+VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
